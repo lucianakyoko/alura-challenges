30b70b5ac232c37f318f3eaa02759f090fa8d75f
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-27 19:27:01 -0300

    styles(frontend/styles): create colors pallet

---
+++ frontend/src/styles/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  primary: {
+    white100: '#FFFFFF',
+    black100: '#464646',
+    black50: '#A2A2A2',
+    black05: '#F5F5F5'
+  },
+  secondary: {
+    blue100: '#2A7AE4',
+    blue80: '#5595E9',
+    blue20: '#D4E4FA'
+  }
+};
