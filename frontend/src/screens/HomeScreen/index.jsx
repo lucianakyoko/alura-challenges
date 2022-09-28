bde0a87d568082d8b14c3d360391b82c20049d2f
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-28 13:35:38 -0300

    feat(components/screens/HomeScreen): add HeroSection component

--- frontend/src/screens/HomeScreen/index.jsx
+++ frontend/src/screens/HomeScreen/index.jsx
@@ -1,10 +1,11 @@
 import { Header } from "../../components/Header";
+import { HeroSection } from "./HeroSection";
 
 export function HomeScreen() {
   return(
     <div>
       <Header />
-
+      <HeroSection />
     </div>
   );
 }
