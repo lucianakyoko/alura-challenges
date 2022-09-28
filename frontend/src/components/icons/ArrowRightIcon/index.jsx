488f8a528239f3b9f3ba69f502ffa2480d7c135e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-28 13:34:08 -0300

    feat(frontend/components/icons): create ArrowRightIcon component

---
+++ frontend/src/components/icons/ArrowRightIcon/index.jsx
@@ -0,0 +1,23 @@
+import { ArrowRightIconContainer } from './style';
+
+export function ArrowRightIcon() {
+  return (
+    <ArrowRightIconContainer
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <g clipPath="url(#clip0_55_1138)">
+        <path
+          fill="#2A7AE4"
+          d="M4 13h12.17l-5.59 5.59L12 20l8-8-8-8-1.41 1.41L16.17 11H4v2z"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_55_1138">
+          <path fill="#fff" d="M0 0H24V24H0z"></path>
+        </clipPath>
+      </defs>
+    </ArrowRightIconContainer>
+  );
+}
---
+++ frontend/src/components/icons/ArrowRightIcon/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ArrowRightIconContainer = styled.svg`
+  width: 1.6rem;
+  height: 1.6rem;
+  
+  @media screen and (min-width: 767px) {
+    width: 2.4rem;
+    height: 2.4rem;   
+  }
+`;
