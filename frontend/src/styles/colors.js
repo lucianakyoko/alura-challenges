7cf7c48eb325da7d722deb8d861efb9e06b54721
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-28 20:42:59 -0300

    styles(frontend/styles): add new colors

--- frontend/src/styles/colors.js
+++ frontend/src/styles/colors.js
@@ -3,11 +3,13 @@ export const colors = {
     white100: '#FFFFFF',
     black100: '#464646',
     black50: '#A2A2A2',
+    black30: '#C8C8C8',
     black05: '#F5F5F5'
   },
   secondary: {
     blue100: '#2A7AE4',
     blue80: '#5595E9',
-    blue20: '#D4E4FA'
+    blue20: '#D4E4FA',
+    blue10: '#EAF2FD',
   }
 };
