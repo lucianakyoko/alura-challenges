c19de88f8dee53b6163bce1baaafeaa81030c716
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-28 20:43:58 -0300

    feat(frontend/components/Footer): create PrimaryFooter component

---
+++ frontend/src/components/Footer/PrimaryFooter/index.jsx
@@ -0,0 +1,48 @@
+import { Logo } from '../../Logo';
+import { Button } from '../../Button';
+
+import {
+  PrimaryFooterContainer,
+  PrimaryFooterContendBox,
+  LogoAndLinksWrapper,
+  FormWrapper,
+  FormContent,
+} from './style';
+
+export function PrimaryFooter() {
+  return (
+    <PrimaryFooterContainer>
+      <PrimaryFooterContendBox>
+        <LogoAndLinksWrapper>
+          <span>
+            <Logo isFixedSize={true} />
+          </span>
+          <ul>
+            <li><a href="">Quem somos nós</a></li>
+            <li><a href="">Política de privacidade</a></li>
+            <li><a href="">Programa fidelidade</a></li>
+            <li><a href="">Nossas lojas</a></li>
+            <li><a href="">Quero ser franqueado</a></li>
+            <li><a href="">Anuncie aqui</a></li>
+          </ul>
+        </LogoAndLinksWrapper>
+
+        <FormWrapper>
+          <p>Fale conosco</p>
+          <FormContent>
+            <div>
+              <label htmlFor="name">Nome</label>
+              <input type="text" name="name" id="name" placeholder='André Lisboa'/>
+            </div>
+
+            <div>
+              <label htmlFor="message">Mensagem</label>
+              <textarea name="message" id="message" placeholder='Escreva sua mensagem'></textarea>
+            </div>
+          </FormContent>
+          <Button title="Enviar mensagem" btnStyle="primary" />
+        </FormWrapper>
+      </PrimaryFooterContendBox>
+    </PrimaryFooterContainer>
+  );
+}
---
+++ frontend/src/components/Footer/PrimaryFooter/style.js
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+import { colors } from "../../../styles/colors";
+
+export const PrimaryFooterContainer = styled.div`
+  padding: 1.6rem;
+  background-color: ${colors.secondary.blue10};
+
+  @media screen and (min-width: 767px) {
+    padding: 3.2rem;
+  }
+`;
+
+export const PrimaryFooterContendBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+
+  @media screen and (min-width: 767px) {
+    flex-direction: row;
+    gap: 1.6rem;
+  }
+
+  @media screen and (min-width: 1130px) {
+    max-width: 113.6rem;
+    margin: auto;
+
+    gap: 11.2rem; 
+  }
+`;
+
+export const LogoAndLinksWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+
+  ul {
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.6rem;
+
+    list-style: none;
+
+    li a {
+      color: ${colors.primary.black100};
+      font-size: 1.6rem;
+      font-weight: 500;
+      line-height: 1.9rem;
+      text-align: center;
+      text-decoration: none;
+    }
+  }
+
+  @media screen and (min-width: 767px) {
+    padding-right: 7.8rem;
+    align-items: flex-start;
+
+    ul {
+      align-items: flex-start;
+      gap: 2.4rem;
+    }
+  }
+
+  @media screen and (min-width: 1130px) {  
+    padding-right: 0;
+    background-color: aliceblue;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 11.2rem;
+
+    span {
+      display: block;
+    }
+
+    ul {
+      width: 17.6rem;
+    }
+  }
+`;
+
+export const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: .8rem;
+
+  p {
+    color: ${colors.primary.black100};
+    font-size: 1.6rem;
+    font-weight: 700;
+    line-height: 1.9rem;
+  }
+
+  @media screen and (min-width: 767px) {
+    width: 100%;
+  }
+`;
+
+export const FormContent = styled.form`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.6rem;
+
+  div {
+    width: 100%;
+    padding:  .8rem 1.2rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: .4rem;
+
+    background-color: ${colors.primary.white100};
+    border-bottom: 1px solid ${colors.primary.black30};
+    border-radius:  .8rem;
+
+    label {
+      color: ${colors.primary.black50};
+      font-size: 1.2rem;
+      font-weight: 400;
+      line-height: 1.6rem;
+    }
+
+    input, textarea {
+      border: none;
+
+      color: ${colors.primary.black100};
+      font-size: 1.6rem;
+      font-weight: 400;
+      font-family: 'Raleway', sans-serif;
+      line-height: 2rem;
+
+      &::focus {
+        outline: none;
+      }
+    }
+  }
+`;
