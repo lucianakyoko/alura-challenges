b4dd67ecde57930e900a61497e6bc6ed7eb6a46b
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-29 14:15:28 -0300

    feat(frontend/screens): create LoginScreen component

---
+++ frontend/src/screens/LoginScreen/index.jsx
@@ -0,0 +1,37 @@
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+import { Button } from '../../components/Button';
+
+import {
+  LoginScreenContainer,
+  LoginFormWrapper,
+  LoginInput,
+} from './style';
+
+export function LoginScreen() {
+  return(
+    <LoginScreenContainer>
+      <Header btnShow={false} />
+
+      <LoginFormWrapper>
+        <h3>Iniciar Sessão</h3>
+        <form action="">
+          <LoginInput type="text" placeholder='Escreva seu email' />
+          <LoginInput type="password" placeholder='Escreva sua senha' />
+        </form>
+
+        <span>
+          <Button 
+            size={'100%'}
+            showBtn 
+            href='/produtos'
+            title='Entrar'
+            btnStyle='primary'
+          />
+        </span>
+      </LoginFormWrapper>
+
+      <Footer />
+    </LoginScreenContainer>
+  );
+}
---
+++ frontend/src/screens/LoginScreen/style.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+import { colors } from '../../styles/colors';
+
+export const LoginScreenContainer = styled.div`
+  background-color: ${colors.primary.black05};
+`;
+
+export const LoginFormWrapper = styled.div`
+  width: 100%;
+  margin: auto;
+  padding: 9.7rem 0 7.6rem 0;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+
+  h3 {
+    color: ${colors.primary.black100};
+    font-size: 1.6rem;
+    font-weight: 700;
+    line-height: 1.9rem;
+    text-align: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1.6rem;
+  }
+
+  span {
+    width: 27.5rem;
+  }
+
+  @media screen and (min-width: 1130px) {
+    gap: 2.45rem;
+
+    form {
+      gap: 2.45rem;
+    }
+    span {
+      width: 42.3rem;
+    }
+  }
+`;
+
+export const LoginInput = styled.input`
+  width: 27.5rem;
+  padding: 1.2rem;
+
+  border: none;
+  border-bottom: 1px solid transparent;
+  border-radius: .4rem;
+
+  color: ${colors.primary.black100};
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 2rem;
+  
+  transition: all .4s ease;
+  &:hover {
+    cursor: text;
+    border-bottom: 1px solid ${colors.primary.black30};
+  }
+  
+  &::placeholder {
+    color: ${colors.primary.black50};
+  }
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid ${colors.primary.black30};
+  }
+
+  @media screen and (min-width: 1130px) {
+    width: 42.3rem;
+  }
+`;
