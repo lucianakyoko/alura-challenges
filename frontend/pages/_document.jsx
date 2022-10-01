6ac2f3371083ec41d9ca3876e871807c2fb67795
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-01 19:04:26 -0300

    feat(frontend): add lang

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="pt-BR">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
