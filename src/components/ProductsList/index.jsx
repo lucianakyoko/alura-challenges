e2a7578ba5f1c412472bf4a6af6aeda21dec6efe
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-12 10:15:08 -0300

    refactor(components/ProductList): removing some line of codes

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -1,50 +1,26 @@
-import { useModal } from '../../hooks/useModal';
 import { getProducts } from '../../datas';
 import { ProductItem } from '../ProductItem';
-import { DeleteMessageModal } from '../DeleteMessageModal';
 
 import {
   ProjectListContainer,
 } from './style';
-import { EditModal } from '../EditModal';
 
-export function ProductList({isAllowed, category}) {
-  const {
-    isDeleteMessageModalOpened,
-    isEditModalOpened,
-    openDeleteMessageModal,
-    closeDeleteMessageModal,
-    openEditModal,
-    closeEditModal,
-  } = useModal();
-
+export function ProductList({category}) {
   const allProducts = getProducts();
   const categoryList = allProducts.filter(item => item.category === `${category}`);
 
   return (
     <ProjectListContainer>
-      {category ? categoryList.map(item => (
+      {categoryList.map(item => (
         <ProductItem 
           key={item.id}
-          isAllowed={isAllowed}
+          id={item.id}
+          item={item.category}
           imgSrc={item.imgSrc} 
           title={item.title} 
           price={item.price} 
         />
-      )) : allProducts.map(item => (
-        <ProductItem 
-          key={item.id}
-          isAllowed={isAllowed}
-          imgSrc={item.imgSrc} 
-          title={item.title} 
-          price={item.price}
-          openDeleteModal={openDeleteMessageModal} 
-          openEditModal={openEditModal} 
-        />
-      ))
-    } 
-      {isDeleteMessageModalOpened ? <DeleteMessageModal closeModal={closeDeleteMessageModal}/> : null}
-      {isEditModalOpened ? <EditModal closeModal={closeEditModal} /> : null }
+      ))} 
     </ProjectListContainer>
   );
 
