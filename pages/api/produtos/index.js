bd282fe8cc689b4b77bf45e71c8ae7a5d6e99e3c
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-12 10:20:22 -0300

    feat(pages/api/produtos): create GET and POST endpoints

---
+++ pages/api/produtos/index.js
@@ -0,0 +1,35 @@
+import dbConnect from "../../../src/utils/dbConnect";
+import Product from "../../../src/models/Product";
+
+
+dbConnect();
+
+export default async (req, res) => {
+  const {method} = req;
+
+  switch(method) {
+    case 'GET':
+      try{
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products});
+
+      } catch (error) {
+        res.status(400).json({success: false});
+      }
+      break;
+    
+    case 'POST':
+      try {
+        const product = await Product.create(req.body);
+        res.status(201).json({success: true, data: product});
+
+      } catch (error) {
+        res.status(400).json({success: false});
+      }
+      break;
+    
+    default: 
+      res.status(400).json({success: false});
+      break;
+  }
+}
