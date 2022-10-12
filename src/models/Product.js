c2952a056198a4083effd47f2fca166453771ad0
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-12 10:19:24 -0300

    feat(models): create Product schema file

---
+++ src/models/Product.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  imgSrc: {type: String, required: true},
+  title: {type: String, required: true},
+  price: {type: String, required: true},
+  category: {type: String, required: true},
+  descrition: {type: String, required: true}
+});
+
+const Product = mongoose.models.Product || mongoose.model('Product', ProductSchema);
+
+export default Product;
