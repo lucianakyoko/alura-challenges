ed92e3e170d2381999eb9db18d7eec6787030b8e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-12 10:07:01 -0300

    feat(screens/AdminDashboardScreen): create Item component

---
+++ src/screens/AdminDashboardScreen/Item/index.jsx
@@ -0,0 +1,69 @@
+import NextLink from "next/link";
+import { useState } from "react";
+import { DeleteIcon } from "../../../components/icons/DeleteIcon";
+import { EditIcon } from "../../../components/icons/EditIcon";
+import { DeleteModal } from "./DeleteModal";
+import { EditModal } from './EditModal'
+
+import {
+  ItemContainer,
+  ItemImageWrapper,
+  ItemIconsWrapper,
+  ItemDetailsWrapper,
+} from './style';
+
+export function Item(props) {
+  const [openDeleteModel, setOpenDeleteModel] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState(false);
+
+  const {
+    img,
+    productName,
+    category,
+    price,
+    href,
+    description
+
+  } = props;
+
+  return(
+    <ItemContainer>
+      <ItemImageWrapper imgSrc={img}>
+        <ItemIconsWrapper>
+          <span onClick={() => setOpenDeleteModel(!openDeleteModel)}><DeleteIcon /></span>
+          <span onClick={() => setOpenEditModal(!openEditModal)}><EditIcon /></span>
+        </ItemIconsWrapper>
+      </ItemImageWrapper>
+
+      <ItemDetailsWrapper>
+        <h3>{productName}</h3>
+        <span>R$ {price}</span>
+        <NextLink href={href}>
+          <a>ver produto</a>
+        </NextLink>
+      </ItemDetailsWrapper>
+      
+      {
+        openDeleteModel && 
+        <DeleteModal 
+          img={img}
+          productName={productName}
+          price={price}
+          setOpenDeleteModel={setOpenDeleteModel}
+        />
+      }
+
+      {
+        openEditModal && 
+        <EditModal 
+          img={img}
+          productName={productName}
+          price={price}
+          category={category}
+          description={description}
+          setOpenEditModal={setOpenEditModal}
+        />
+      }
+    </ItemContainer>
+  );
+}
---
+++ src/screens/AdminDashboardScreen/Item/style.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import { colors } from "../../../styles/colors";
+
+export const ItemContainer = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: .8rem;
+`;
+
+export const ItemImageWrapper = styled.div`
+  width: 15.6rem;
+  height: 17.4rem;
+
+  background-image: url(${props => props.imgSrc});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  position: relative;
+
+  @media screen and (min-width: 767px) {
+    width: 16.4rem;
+  }
+
+  @media screen and (min-width: 1130px) {
+    width: 17.6rem;
+  }
+`;
+
+export const ItemDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap:  .8rem;
+  h3 {
+    color: ${colors.primary.black100};
+    font-size: 1.4rem;
+    font-weight: 500;
+    line-height: 1.6rem;
+  }
+
+  span {
+    color: ${colors.primary.black100};
+    font-size: 1.6rem;
+    font-weight: 700;
+  }
+
+  a {
+    display: block;
+    color: ${colors.secondary.blue100};
+    font-size: 1.4rem;
+    font-weight: 700;
+    line-height: 1.6rem;
+    text-decoration: none;
+  }
+`;
+
+export const ItemIconsWrapper = styled.div`
+  position: absolute;
+  top: .8rem;
+  right: .8rem;
+
+  display: flex;
+  align-items: center;
+  gap: 1.6rem;
+`;
