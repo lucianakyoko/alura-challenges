5a8a4b5054b17807c3c62aa5e80cfd9b0599bf39
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-14 21:05:05 -0300

    refactor(models/Product): refactoring code

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,11 +1,26 @@
 import mongoose from 'mongoose';
 
 const ProductSchema = new mongoose.Schema({
-  imgSrc: {type: String, required: true},
-  title: {type: String, required: true},
-  price: {type: String, required: true},
-  category: {type: String, required: true},
-  descrition: {type: String, required: true}
+  imgSrc: {
+    type: String, 
+    required: true
+  },
+  title: {
+    type: String, 
+    required: true
+  },
+  price: {
+    type: String, 
+    required: true
+  },
+  category: {
+    type: String, 
+    required: true
+  },
+  descrition: {
+    type: String, 
+    required: true
+  }
 });
 
 const Product = mongoose.models.Product || mongoose.model('Product', ProductSchema);
