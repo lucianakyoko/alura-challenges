18c28d823b2c77c3f20c40ea16121e1136b2432d
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-15 16:39:12 -0300

    feat(screens/AllProducts): create category page

--- src/components/UI/ProductsSectionUI/index.jsx
+++ src/components/UI/ProductsSectionUI/index.jsx
@@ -6,12 +6,14 @@ import {
 } from './style';
 
 export function ProductsSectionUI({title, children, showMoreBtn}) {
+  const categoryHref = title.replace(/\s+/g, '-').toLowerCase();;
+
   return(
     <HomeSectionUIContainer>
       <HomeSectionUIHeader>
         <h2>{title}</h2>
         <div className={showMoreBtn ? '' : 'dont-show-btn'}>
-          <NextLink href='/produtos'>
+          <NextLink href={`/produtos`} as={`/${categoryHref}`}>
             <a>ver tudo</a>
           </NextLink>
           <span>
--- src/screens/AllProducts/index.jsx
+++ src/screens/AllProducts/index.jsx
@@ -1,27 +1,38 @@
+import { useRouter } from 'next/router';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { ProductItem } from '../../components/ProductItem';
+import { ProductList } from '../../components/ProductsList';
+import { ProductsSectionUI } from '../../components/UI/ProductsSectionUI';
 
 import {
   AllProductsContainer,
 } from './style';
 
 export function AllProducts({products}) {
+  const router = useRouter();
+  const categoryLink = router.asPath;
+  const category = categoryLink.replace(/[/]/g, '');
+  const categoryFormated = category.replace(/[-]/g, ' ')
+  const categoryProducts = products.filter(product => product.category === categoryFormated)
+
   return (
     <>
       <Header />
 
       <AllProductsContainer>
-        {products.map(product => (
-          <ProductItem 
-            key={product._id}
-            id={product._id}
-            imgSrc={product.imgSrc}
-            title={product.title}
-            price={product.price}
-          />
+        <ProductsSectionUI
+          title={categoryFormated}
+        >
 
-        ))}
+        {
+          <ProductList
+            category={categoryFormated}
+            productsList={categoryProducts}
+          />
+        }
+
+        </ProductsSectionUI>
       </AllProductsContainer>
       <Footer />
     </>
