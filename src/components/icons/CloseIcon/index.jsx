a17cd39d6486eef12ae75393aedfc5a1176477fa
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-18 15:38:53 -0300

    feat(components/Icons): create CloseIcon component

---
+++ src/components/icons/CloseIcon/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function CloseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 16 16"
+    >
+      <path
+        stroke="#464646"
+        strokeLinecap="round"
+        strokeWidth="2"
+        d="M1 1l14 14M1 15L15 1"
+      ></path>
+    </svg>
+  );
+}
