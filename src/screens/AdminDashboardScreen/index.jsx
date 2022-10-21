804babb9b9b50be351d4f4d5660f523361b0d72f
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-21 20:54:46 -0300

    feat(screens/AdminDashboardScreen): add authentication verification

--- src/screens/AdminDashboardScreen/index.jsx
+++ src/screens/AdminDashboardScreen/index.jsx
@@ -1,7 +1,13 @@
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
 import { Button } from "../../components/Button";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { ToastNotification } from "../../components/ToastNotification";
 import { ItemsList } from "./ItemsList";
+import { Loader } from '../../components/Loader';
 
 import {
   AdminDashboardScreenContainer,
@@ -10,29 +16,51 @@ import {
 }  from './style';
 
 export function AdminDashboardScreen({products}) {
+  const router = useRouter();
+  const [toastIsOpen, setToastIsOpen] = useState(true);
+  const { status } = useSession();
 
-  return(
-    <AdminDashboardScreenContainer>
-      <Header 
-        showSearchBar
-        productsList={products}
-      />
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/auth/login")
+    };
+  }, [status]);
+  
+  if (status === "authenticated") {
+    setInterval(() => {
+      setToastIsOpen(false);
+    }, 3300);
 
-      <AdminDashboardContentWrapper>
-        <AdminDashboardTitleAndBtnWrapper>
-          <h3>Todos os produtos</h3>
-          <Button 
-            showBtn
-            href='/adicionar-produto'
-            title='Adicionar produto'
-            btnStyle='primary'
-          />
-        </AdminDashboardTitleAndBtnWrapper>
-
-        <ItemsList products={products} />
-      </AdminDashboardContentWrapper>
-
-      <Footer />
-    </AdminDashboardScreenContainer>
-  )
+    return(
+      <AdminDashboardScreenContainer>
+        <Header 
+          showSearchBar
+          productsList={products}
+        />
+  
+        <AdminDashboardContentWrapper>
+          <AdminDashboardTitleAndBtnWrapper>
+            <h3>Todos os produtos</h3>
+            <Button 
+              showBtn
+              href='/adicionar-produto'
+              title='Adicionar produto'
+              btnStyle='primary'
+            />
+          </AdminDashboardTitleAndBtnWrapper>
+  
+          <ItemsList products={products} />
+        </AdminDashboardContentWrapper>
+  
+        <ToastNotification toastIsOpen={toastIsOpen} setToastIsOpen={setToastIsOpen} />
+        <Footer />
+      </AdminDashboardScreenContainer>
+    )
+  }
+  
+  if(status === 'loading') {
+    return(
+      <Loader />
+    )
+  }
 }
