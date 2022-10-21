2db337865b29ee98d4312b680d2e468c446871d5
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-21 20:46:35 -0300

    feat(pages/api/auth): create Next auth credential provider

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialProvider({
+      name: "credentials",
+      credentials: {},
+      authorize: (credentials, req) => {
+        const {email , password} = credentials;
+
+        if(email !== process.env.LOGIN || password !== process.env.PASSWORD) {
+          throw new Error('Invalid credentials')
+        }
+        return {
+          id: '1234',
+          name: 'Admin',
+          email: 'admin@email.com'
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+});
