0da7ef5df1a2ca56b53bcf5c044b17c459686b8e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-10-22 15:26:28 -0300

    feat(components/icons): create MailIcon component

---
+++ src/components/icons/MailIcon/index.jsx
@@ -0,0 +1,26 @@
+export function MailIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#2A7AE4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2z"
+      ></path>
+      <path
+        stroke="#2A7AE4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M22 7l-8.97 5.7a1.94 1.94 0 01-2.06 0L2 7"
+      ></path>
+    </svg>
+  );
+}
