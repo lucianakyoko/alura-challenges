852102678f2125c8414a1b52a74dab5585ec0767
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2023-03-03 21:06:52 -0300

    feat(components): create PageLayout component

---
+++ codechella/src/components/PageLayout/index.jsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { Calistoga } from "next/font/google";
+import { Header } from "@/components/Header";
+import { Banner } from "../Banner/";
+// import { PageHero } from "@/components/PageHero/index.jsx";
+import { Footer } from "@/components/Footer";
+
+import {
+  PageLayoutContainer,
+} from './style.js';
+
+export const calistogaFont = Calistoga({
+  weight: ['400'],
+  subsets: ['latin']
+});
+
+export function PageLayout({pageTitle, heroText, page, children}) {
+
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <PageLayoutContainer>
+        <Header />
+        <Banner heroText={heroText} page={page} />
+
+        {children}
+
+        <Footer />
+      </PageLayoutContainer>
+    </>
+  );
+}
