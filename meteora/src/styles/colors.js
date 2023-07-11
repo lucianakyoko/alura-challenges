d92521d4f0629679fe9c99b1bcae58e868bebbe8
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2023-07-11 20:43:38 -0300

    styles(meteora/styles): add new color

--- meteora/src/styles/colors.js
+++ meteora/src/styles/colors.js
@@ -2,10 +2,12 @@ export const colors = {
   black: '#000000',
   border: 'rgba(0, 0, 0, 0.13)',
   button: '#9353FF',
+  danger: '#FF0000',
   highlight: '#DAFF01',
   line: '#D8D8D8',
   placeholder: '#6C757D',
   smallMenuBackground: '#343A40',
   text: '#212529',
+  textLight: '#ADB5BD',
   white: '#FFFFFF',
 }
